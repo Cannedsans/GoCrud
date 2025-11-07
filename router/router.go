3ef7f1ba9615bdf5bfde995e2729eb29452f7474
João Felipe <joaofilipera13@gmail.com>
2025-11-06 21:20:47 -0300

    feat: add routes logic

--- router/router.go
+++ router/router.go
@@ -3,12 +3,9 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize(){
-	r := gin.Default()
-	r.GET("/",func(ctx *gin.Context) {
-		ctx.JSON(200,gin.H{
-			"Message" : "Olá mundo",
-		})
-	})
+	router := gin.Default()
 
-	r.Run()
+	initializeRoutes(router)
+
+	router.Run()
 }
