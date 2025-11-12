8f2c5c72235d060f38c56d31786bbf50da566b48
João Felipe <joaofilipera13@gmail.com>
2025-11-12 13:52:14 -0300

    feat: add request and response helpers

---
+++ handler/request.go
@@ -0,0 +1,43 @@
+package handler
+
+import "fmt"
+
+func errParamIsRequired(name, typ string) error{
+	return fmt.Errorf("param: %s (type: %s) is missing", name,typ)
+}
+
+type CreateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool   `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *CreateOpeningRequest) Validade() error{
+	if r == nil{
+		return fmt.Errorf("request body está vazio")
+	}
+
+	if r.Role == ""{
+		return errParamIsRequired(r.Role, "string")
+	}
+	if r.Company == ""{
+		return errParamIsRequired("Company", "string")
+	}
+	if r.Location == ""{
+		return errParamIsRequired("Location", "string")
+	}
+	if r.Link == ""{
+		return errParamIsRequired("Link", "string")
+	}
+	if r.Remote == nil{
+		return errParamIsRequired("Remote", "Boolean")
+	}
+	if r.Salary <= 0{
+		return errParamIsRequired("Salary", "int64")
+	}
+
+	return nil
+}
